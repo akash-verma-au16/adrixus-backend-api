e085bece95f3ef1efe87d239d9916ff14ec50a2c
Akash Verma <akashcool2014.av@gmail.com>
2021-07-24 23:12:34 +0530

    backend_api

---
+++ routes/auth.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const User = require('../models/User');
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return res.json({ msg: 'Invalid Credentials' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.json({ msg: 'Invalid Credentials' });
+    }
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const axios = require('axios');
+const User = require('../models/User');
+const auth = require('../middleware/auth');
+const jsonData = require('../config/raw');
+
+router.post('/signup', async (req, res) => {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      return res.json({ msg: 'User already exists' });
+    }
+
+    user = new User(req.body);
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(req.body.password, salt);
+    await user.save();
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+router.get('/users', auth, (req, res) => {
+  // Just to get a 100 users I have used raw json data
+  res.json(jsonData);
+});
+
+module.exports = router;
