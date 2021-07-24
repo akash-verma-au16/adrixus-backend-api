8718f150fc14a78abcd11fd54f37990a97b2ef6b
Akash Verma <akashcool2014.av@gmail.com>
2021-07-24 23:36:42 +0530

    finished api

--- server.js
+++ server.js
@@ -11,5 +11,12 @@ app.use(express.json({ extended: false }));
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/auth'));
 
+app.get('*', (req, res) => {
+  res.json({
+    msg: 'API deployed visit UI through link',
+    link: 'https://adrixus-app.netlify.app/',
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
